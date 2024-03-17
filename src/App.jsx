ca9476a671ba2066ebb0c0de72b09cd4528d4fa0
Oronno Akter Usha <ushaoronno1@gmail.com>
2024-03-17 14:32:14 +0600

    added fake data

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css'
+import Blogs from './components/Header/Blogs/Blogs'
+import Header from './components/Header/Header'
 
 function App() {
 
   return (
     <>
-     
-      <h1 className='text-6xl bg-red-300'>Knowledge Cafe</h1>
-      
+      <Header></Header>
+      <Blogs></Blogs>
     </>
   )
 }
